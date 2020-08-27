6fe410f38eb58970615968dbe6af8ffd31ac46f3
Jooyong Yi <jooyong@unist.ac.kr>
2020-08-27 13:18:09 +0000

    added a new test into PropagationTest

--- fuzz/src/test/java/edu/berkeley/cs/jqf/fuzz/prop/PropagationTest.java
+++ fuzz/src/test/java/edu/berkeley/cs/jqf/fuzz/prop/PropagationTest.java
@@ -87,4 +87,36 @@ public class PropagationTest {
                 "../src/test/resources/patches/Patch27/Math2p/target/test-classes:../src/test/resources/patches/Patch27/Math2p/target/classes",
                 "org.apache.commons.math3.distribution.JQF_HypergeometricDistributionTest", "testMath1021"});
     }
+
+    @Test
+    public void runZestCLI2_patch197() throws IOException {
+        Path fuzz_results_patch_dir = FileSystems.getDefault().getPath("..", "src", "test", "resources", "fuzz-results-patch");
+        Path log_dir = FileSystems.getDefault().getPath("..", "src", "test", "resources", "log");
+
+        if (fuzz_results_patch_dir.toFile().exists()) {
+            Files.walk(fuzz_results_patch_dir)
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        }
+        if (log_dir.toFile().exists()) {
+            Files.walk(log_dir)
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+        }
+
+        ZestCLI2.main(new String[] {
+                "--target", "org/apache/commons/math3/optimization/fitting/HarmonicFitter.java:327",
+                "--save-all-inputs",
+                "--logdir", "../src/test/resources/log",
+                "--seed", "885441",
+                "--max-corpus-size", "10",
+                "--plateau-threshold", "10",
+                "--verbose",
+                "-o", "../src/test/resources/fuzz-results-patch",
+                "../src/test/resources/patches/Patch197/Math25b/target/test-classes:../src/test/resources/patches/Patch197/Math25b/target/classes",
+                "../src/test/resources/patches/Patch197/Math25p/target/test-classes:../src/test/resources/patches/Patch197/Math25p/target/classes",
+                "org.apache.commons.math3.optimization.fitting.JQF_HarmonicFitterTest", "testMath844"});
+    }
 }
