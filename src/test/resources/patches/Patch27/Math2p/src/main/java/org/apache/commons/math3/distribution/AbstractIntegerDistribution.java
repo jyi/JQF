c35f3916c9c7486bbc9bd458abc4a48bc407907b
Jooyong Yi <jooyong@unist.ac.kr>
2020-09-17 01:47:47 +0000

    push back the original patch 27

--- src/test/resources/patches/Patch27/Math2p/src/main/java/org/apache/commons/math3/distribution/AbstractIntegerDistribution.java
+++ src/test/resources/patches/Patch27/Math2p/src/main/java/org/apache/commons/math3/distribution/AbstractIntegerDistribution.java
@@ -98,9 +98,6 @@ public abstract class AbstractIntegerDistribution implements IntegerDistribution
      * </ul>
      */
     public int inverseCumulativeProbability(final double p) throws OutOfRangeException {
-        if(true){
-            return 20;
-        }
         if (p < 0.0 || p > 1.0) {
             throw new OutOfRangeException(p, 0, 1);
         }
@@ -138,8 +135,8 @@ public abstract class AbstractIntegerDistribution implements IntegerDistribution
             k = 1.0 / k;
             tmp = mu + k * sigma;
             if (tmp < upper) {
-		    if (System.out != null) System.currentTimeMillis();
-                tmp = mu + k * sigma;
+		 if (System.out != null) System.currentTimeMillis();
+                tmp = mu + (k * sigma);
             }
         }
 
